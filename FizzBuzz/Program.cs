6ab96ccbe5673105b501ee62a3ce63eebee35486
cbm888 <cbmarch88@gmail.com>
2021-09-02 14:21:39 -0500

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("How many units of fizzing and buzzing do you need in your life?");
+            string input = Console.ReadLine();
+            Int32.TryParse(input, out int num);
+            
+            for (int i = 1; i < num; i++)
+            {
+                if (!(i % 5 == 0 || i % 3 == 0))
+                {
+                    Console.WriteLine(i);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+            }
+            Console.WriteLine("TRADITION!!!!!");
+
+        }
+    }
+}
